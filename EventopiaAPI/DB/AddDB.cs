4cdc0f9b0aee993e99def1fbfc9ee197cb749664
DenisaGal <62429107+DenisaGal@users.noreply.github.com>
2023-12-04 18:03:55 +0200

    Initial steps for DB and events table created.

---
+++ EventopiaAPI/DB/AddDB.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EventopiaAPI.DB
+{
+    public class EventopoiaDBContext:DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public EventopoiaDBContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("EventopiaDB"));
+        }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
